70743d671fee746533aa7d707137ffe529b8b492
Pavel <pavel.volniy.2020@gmail.com>
2022-04-25 13:57:58 +0300

    Fix problems? injection ROOM

--- app/src/main/java/com/example/shoplist/data/ShopItemDAO.kt
+++ app/src/main/java/com/example/shoplist/data/ShopItemDAO.kt
@@ -1,5 +1,6 @@
 package com.example.shoplist.data
 
+import androidx.lifecycle.MutableLiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -9,13 +10,13 @@ import com.example.shoplist.domain.ShopItem
 @Dao
 interface ShopItemDAO {
     @Query("SELECT * FROM ShopItemData")
-    fun getShopItemList(): List<ShopItem>
+    fun getShopItemList(): MutableLiveData<List<ShopItem>>
 
     @Query("SELECT * FROM ShopItemData WHERE shopItemId in(:id)")
-    fun getShopItemById(id: Int): List<ShopItem>
+    fun getShopItemById(id: Int): ShopItem
 
     @Insert
-    fun insertShopItem(vararg shopItem: ShopItem)
+    fun insertShopItem( shopItem: ShopItem)
 
     @Delete
     fun deleteShopItem(shopItem: ShopItem)
--- app/src/main/java/com/example/shoplist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shoplist/domain/ShopListRepository.kt
@@ -1,12 +1,18 @@
 package com.example.shoplist.domain
 
 import androidx.lifecycle.LiveData
+import androidx.room.*
 
+@Dao
 interface ShopListRepository {
-
+    @Insert
     fun addShopItem(shopItem: ShopItem)
+    @Delete
     fun deleteShopItem(shopItem: ShopItem)
+    @Update
     fun editShopItem(shopItem: ShopItem)
+    @Query("SELECT * FROM ShopItem WHERE id IN (:shopItemId)")
     fun getShopItem(shopItemId: Int): ShopItem
+    @Query("SELECT * FROM ShopItem")
     fun getShopList(): LiveData<List<ShopItem>>
 }
